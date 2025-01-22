f46cbe593456e43e2f99b7af85d961fe6740ab0b
mgrkan <fogrkan@gmail.com>
2025-01-22 21:32:33 +0300

    changed project structure, remover Dockerfile and docker-compose file. Queueing and image processing works

--- remover/src/worker.py
+++ remover/src/worker.py
@@ -4,21 +4,22 @@ import remover
 from models import RembgModel
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
     channel = connection.channel()
-    channel.queue_declare(queue='hello', durable=True)
+    channel.queue_declare(queue='image_queue', durable=True)
 
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
         try:
-            remover.process_image(body, 'output.png', RembgModel())
+            remover.process_image("/app/src/" + body.decode("utf-8"), '/app/images/output.png', RembgModel())
             print(" [x] Done")
             ch.basic_ack(delivery_tag = method.delivery_tag)
         except Exception as e:
             print(f" [x] Error: {e}")
             ch.basic_reject(delivery_tag = method.delivery_tag, requeue=False)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback)
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='image_queue', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- task.py
+++ task.py
@@ -4,10 +4,10 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello', durable=True)
+channel.queue_declare(queue='image_queue', durable=True)
 
 channel.basic_publish(exchange='',
-                      routing_key='hello',
+                      routing_key='image_queue',
                       body='sample1.jpeg',
                       properties=pika.BasicProperties(
                          delivery_mode = pika.DeliveryMode.Persistent, # make message persistent
