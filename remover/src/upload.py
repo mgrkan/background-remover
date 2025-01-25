d2d236cc13c7884b58a26d24eb4ee3438ff1e2ed
mgrkan <fogrkan@gmail.com>
2025-01-25 16:00:22 +0300

    changed healthchecks and images bucket access

--- remover/src/upload.py
+++ remover/src/upload.py
@@ -8,10 +8,12 @@ s3 = boto3.client('s3',
                   endpoint_url='http://minio:9000', 
                   aws_access_key_id=aaki, 
                   aws_secret_access_key=asak)
+"""
 try:
     s3.create_bucket(Bucket=bucket_name)
 except:
     pass
-
+"""
 def upload_file(file_name, object_name):
-    s3.upload_file(file_name, bucket_name, object_name)
+    extra_args = {'ACL': 'public-read', 'ContentType': 'image/png', 'ContentDisposition': 'inline'}
+    s3.upload_file(file_name, bucket_name, object_name, ExtraArgs=extra_args)
